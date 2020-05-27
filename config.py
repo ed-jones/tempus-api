931f842d1e2198902bf2bbdc334c0db3481cc056
Ed Jones <edjones1999@gmail.com>
2020-05-27 16:53:38 +1000

    Removed venv folder

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'imULe11*jU'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mQ84*!A@149.28.179.78:5432/csci334'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://tempus:%yLb36Bs2G@127.0.0.1:5432/tempus'
+    DATABASE_PASSWORD = '%yLb36Bs2G'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tempus:'+DATABASE_PASSWORD+':@127.0.0.1:5432/tempus'
